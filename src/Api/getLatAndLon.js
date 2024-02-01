6c7a1010aeeb53e72f01f5a3ae927d94429b11ff
AgentRittik <109760373+AgentRittik@users.noreply.github.com>
2024-02-01 21:22:49 +0530

    changes in api url

--- src/Api/getLatAndLon.js
+++ src/Api/getLatAndLon.js
@@ -5,7 +5,7 @@ import axios from "axios";
 
 async function getLatAndLon(cityName) {
     try {
-        const response = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${import.meta.env.VITE_WEATHER_API_KEY}`);
+        const response = await axios.get(`https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${import.meta.env.VITE_WEATHER_API_KEY}`);
         console.log(response);
         return response.data[0];
     } catch (error) {
